dba8a135dcd18819bd70c62f26c52e5de713cfbd
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-09-01 23:52:05 +0300

    health check method in dealer model

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,2 +1,10 @@
 class Dealer < ActiveRecord::Base
+
+  def self.health_check
+    all.each do |dealer|
+      http = Net::HTTP.new("www.hotspring.co.uk/")
+      response = http.request_head('/')
+      dealer.update_attribute(:status, response.status)
+    end
+  end
 end
