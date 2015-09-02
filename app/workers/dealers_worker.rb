bdf26b189bbfe54ca5d1f402c76014f8b27f1a5f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-09-02 23:22:24 +0300

    mailer called in dealer worker

--- app/workers/dealers_worker.rb
+++ app/workers/dealers_worker.rb
@@ -2,7 +2,7 @@ class DealersWorker
   include Sidekiq::Worker
   sidekiq_options queue: "dealers"
   # sidekiq_options retry: false
-  
+
   def perform()
     Dealer.empty_statuses
     dealers = Dealer.all.each do |dealer|
@@ -10,6 +10,7 @@ class DealersWorker
       response = Net::HTTP.get_response(uri)
       status = response.inspect.match(/\w+[0-9] [a-zA-Z ]+ /)
       dealer.update_attribute(:status, status)
+      DealerMailer.failed_path(dealer).deliver_later if status != '200 OK '
     end
   end
 end
