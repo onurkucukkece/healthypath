b1156a561d4099529a0b68a5fb861dec0c9ed9b1
IST@Ease Development <development@istatease.com>
2015-09-03 13:40:36 +0300

    website field implemented

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -69,6 +69,6 @@ class DealersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dealer_params
-      params.require(:dealer).permit(:name, :path, :status)
+      params.require(:dealer).permit(:name, :path, :status, :website)
     end
 end
--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,4 +1,5 @@
 class Dealer < ActiveRecord::Base
+  WEBSITES = %w(www.hotspring.co.uk www.calderaspas.co.uk)
   def self.empty_statuses
     all.each do |dealer|
       dealer.update_attribute(:status, '')
