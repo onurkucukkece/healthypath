2a72ad91628cc2e06bdcd1783a2c81b937242c86
IST@Ease Development <development@istatease.com>
2015-09-03 13:30:21 +0300

    added website column to dealers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903101522) do
+ActiveRecord::Schema.define(version: 20150903102956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150903101522) do
     t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "website"
   end
 
   create_table "users", force: :cascade do |t|
